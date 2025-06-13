aff10ba319fb334c0852f0ee952297b11898fdf9
AregGevorgyan <109322954+AregGevorgyan@users.noreply.github.com>
2025-06-13 02:19:43 +0000

    Deploying to gh-pages from @ AregGevorgyan/AregGevorgyan.github.io@5049ded484c06e10337f75487f4d97bdad8dbdc7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -376,15 +376,10 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-hi-i-just-created-this-website-and-it-is-still-under-construction",
+          title: 'Hi, I just created this website and it is still under construction!',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "projects-project-1",
+          section: "News",},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
